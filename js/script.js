a71f4f6bc2989843f492ce4de9efc5852bcc2e5f
kenneth-gouw <159185824+kenneth-gouw@users.noreply.github.com>
2024-05-23 18:34:55 +0000

    sssss

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Kenneth All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: May  2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+let clicks = parseInt(localStorage.getItem("click-counter")) || 0
+
+function updateCookieCount() {
+
+  document.getElementById("click-count").innerHTML = clicks
+}
+
+function myButtonClicked() {
+  clicks += 1
+
+  // Update the HTML element with the updated clicks value
+  document.getElementById("click-count").innerHTML = clicks
+
+  // Update the clicks value in local storage
+  localStorage.setItem("click-counter", clicks)
+}
